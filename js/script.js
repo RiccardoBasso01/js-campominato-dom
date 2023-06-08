75c5f902fae1cfe9f4b3275cb184abfcc77a5c27
RiccardoBasso01 <riccardo.basso2001@gmail.com>
2023-06-08 18:40:54 +0200

    Alert vinto/perso

--- js/script.js
+++ js/script.js
@@ -5,14 +5,20 @@ const grid = document.getElementById('grid'); // GRIGLIA
 const play = document.getElementById('play'); // BOTTONE
 const difficulty = document.getElementById('difficulty'); // DIFFICOLTÁ
 const bombNumber = document.getElementById('bomb-number'); // Numero di BOMBE
-// Numero di celle
-let numberOfCell;
-let numberOfBomb;
+const score = document.getElementById('score'); // Punteggio
+
+let numberOfCell; // Numero di celle
+let numberOfBomb; // Numero di bombe
+let currentScore = 0; // Punteggio
 
 // Al click del bottone gioca
 play.addEventListener('click', function () {
     // Faccio apparire la griglia
     grid.classList.remove('d-none');
+
+    // Pulisco la griglia prima di generare le celle nuove
+    grid.innerText = '';
+
 
     if (difficulty.value === 'easy') {
         grid.classList.add('easy');
@@ -45,83 +51,107 @@ play.addEventListener('click', function () {
 
     console.log('Numero di celle: ', numberOfCell)
 
-    // Elimino le "vecchie celle" prima di generare le nuove
-    grid.innerText = '';
+    // FUNZIOINE PER GENRARE LE CELLE
+    const getBomb = () => {
+        const bomb = []; // Array con i numeri random delle caselle-bomba
+        let randomNumber;
+        for (let i = 0; i < numberOfBomb; i++) {
+            do {
+                randomNumber = Math.floor((Math.random() * numberOfCell) + 1);
+            } while (bomb.includes(randomNumber)) // Estrae tot numeri tutti diversi tra loro
 
+            bomb.push(randomNumber);
+        }
+        return bomb;
+    }
+    // Genero le bombe
+    const bombs = getBomb();
+    console.log('I numeri delle caselle-bomba sono: ', bombs);
+
+
+
+    // FUNZIOINE PER GENRARE LE CELLE
+    const getCell = (numberOfCell, container) => {
+        for (let i = 0; i < numberOfCell; i++) container.innerHTML += `<div class="cell"></div>`;
+        return container;
+    }
     // Genero le celle
-    for (let i = 0; i < numberOfCell; i++) grid.innerHTML += `<div class="cell"></div>`;
+    getCell(numberOfCell, grid);
+    const cells = document.querySelectorAll('.cell'); // CELLE
 
-    const cell = document.querySelectorAll('.cell'); // CELLE
-    console.log('Numero di celle: ', cell.length);
+    //Assegno alle celle-bomba la classe bomb
+    for (let i = 0; i < bombs.length; i++) {
+        cells[bombs[i] - 1].classList.add('bomb', 'disable-bomb');
+        cells[bombs[i] - 1].innerText = '';
 
-    // Assegno i numeri in sequenza a tutte le celle
-    for (let i = 0; i < cell.length; i++) {
-        cell[i].innerText = i + 1;
-
-
-        //! Al click sulle celle
-        cell[i].addEventListener('click', function () {
-            cell[i].classList.toggle('active');
-            const x = i + 1;// Numero della cella cliccata
-            console.log('Cliccata cella numero: ', x);
-
-            // Genera celle con le bombe
-            // Array con i numeri random
-            const bomb = [];
-            let randomNumber;
-            for (let i = 0; i < numberOfBomb; i++) {
-                do {
-                    randomNumber = Math.floor((Math.random() * cell.length) + 1);
-                } while (bomb.includes(randomNumber) || randomNumber === x) // Estrae tot numeri tutti diversi tra loro e diversi dal numero della cella cliccata
-
-                bomb.push(randomNumber);
-            }
-            console.table(bomb);
-
-            for (let i = 0; i < bomb.length; i++) {
-                console.log(bomb[i])
-                cell[bomb[i] - 1].classList.add('bomb');
-                cell[bomb[i] - 1].innerText = '';
-            }
-
-            // Controllo di quante bombe ci sono intorno alla cella selezionata
-            const bombArray = [cell[i - (Math.sqrt(cell.length) - 1)].className, cell[i - (Math.sqrt(cell.length))].className, cell[i - (Math.sqrt(cell.length) + 1)].className, cell[i - 1].className, cell[i].className, cell[i + 1].className, cell[i + (Math.sqrt(cell.length) - 1)].className, cell[i + (Math.sqrt(cell.length))].className, cell[i + (Math.sqrt(cell.length) + 1)].className]
-            console.table(bombArray)
-            console.log((Math.sqrt(cell.length)))
-            console.log(cell[i])
-            console.log('i uguale ', i)
-            console.log(cell[i - (Math.sqrt(cell.length))].className)
-
-            let counter = 0;
-            for (let i = 0; i < bombArray.length; i++) {
-                if (bombArray[i] === 'cell bomb') {
-                    counter += 1;
-                }
-            }
-
-            // Numero delle bombe adiacenti alla cella cliccata
-            if (counter === 1) {
-                cell[i].classList.add('one-bomb');
-                cell[i].innerText = '';
-            } else if (counter === 2) {
-                cell[i].classList.add('two-bomb');
-                cell[i].innerText = '';
-            } else if (counter === 3) {
-                cell[i].classList.add('three-bomb');
-                cell[i].innerText = '';
-            } else if (counter === 4) {
-                cell[i].classList.add('for-bomb');
-                cell[i].innerText = '';
-            } else if (counter === 5) {
-                cell[i].classList.add('five-bomb');
-                cell[i].innerText = '';
-            } else if (counter === 6) {
-                cell[i].classList.add('six-bomb');
-                cell[i].innerText = '';
-            }
-            console.log('Contatore: ', counter)
-
-        })
+        console.log('Celle bomba: ', bombs[i])
     }
 
-}) 
+    const endGame = (result) => {
+        for(let i = 0; i < cells.length; i++){
+            cells[i].classList.remove('disable-bomb')
+        }
+        if (result === 'win'){
+            alert('Hai vinto!')
+        } else{
+            alert('Hai perso!')
+        }
+    }
+
+    // ciclo for per selezionare le celle all' interno dell' array cells
+    for (let i = 0; i < cells.length; i++) {
+        const currentCell = cells[i];
+        // ! Al click sulle celle
+        currentCell.addEventListener('click', function () {
+            const isBomb = bombs.includes(i + 1)
+            if(isBomb){
+                cells[i].classList.remove('disable-bomb');
+                endGame('lose');
+            }else if(!(currentCell.classList.contains('active'))){
+                currentCell.classList.add('active');
+                score.innerText = ++currentScore;
+                console.log(currentScore)
+            }
+            
+            if(currentScore === (numberOfCell - numberOfBomb)){
+                endGame('win');
+            }
+
+                // // Controllo di quante bombe ci sono intorno alla cella selezionata
+                // const bombArray = [cells[i - (Math.sqrt(cells.length) + 1)].className, cells[i - (Math.sqrt(cells.length))].className, cells[i - (Math.sqrt(cells.length) - 1)].className, cells[i - 1].className, cells[i].className, cells[i + 1].className, cells[i + (Math.sqrt(cells.length) - 1)].className, cells[i + (Math.sqrt(cells.length))].className, cells[i + (Math.sqrt(cells.length) + 1)].className]
+                // console.table(bombArray)
+                // console.log('Numero delle colonne: ', (Math.sqrt(cells.length)))
+
+                // let counter = 0;
+                // for (let i = 0; i < bombArray.length; i++) {
+                //     if (bombArray[i] === 'cell bomb' || bombArray[i] === 'cell bomb disable-bomb') {
+                //         counter += 1;
+                //     }
+                // }
+
+                // // Numero delle bombe adiacenti alla cella cliccata
+                // if (counter === 1) {
+                //     cells[i].classList.add('one-bomb');
+                //     cells[i].innerText = '';
+                // } else if (counter === 2) {
+                //     cells[i].classList.add('two-bomb');
+                //     cells[i].innerText = '';
+                // } else if (counter === 3) {
+                //     cells[i].classList.add('three-bomb');
+                //     cells[i].innerText = '';
+                // } else if (counter === 4) {
+                //     cells[i].classList.add('for-bomb');
+                //     cells[i].innerText = '';
+                // } else if (counter === 5) {
+                //     cells[i].classList.add('five-bomb');
+                //     cells[i].innerText = '';
+                // } else if (counter === 6) {
+                //     cells[i].classList.add('six-bomb');
+                //     cells[i].innerText = '';
+                // }
+                // console.log('Contatore: ', counter)
+            
+        })
+    }
+                    
+})
